77b04eb1a4febaf9365f793554b6ccda233a2a66
Marko Petric <markopetric.fer@gmail.com>
2017-11-10 23:38:20 +0100

    Added the background color feature.

--- designs.js
+++ designs.js
@@ -6,6 +6,10 @@
 //create constants and initialize them for colorPicker and sizePicker
 const sizePicker = $('#sizePicker');
 const colorPicker = $('#colorPicker');
+const button = $('#button');
+const colorPickerBackground = $('#colorPickerBackground');
+
+let backgroundColor;
 
 
 // helper function that transforms hex colors to rbg colors for easiser conditions later on
@@ -47,21 +51,50 @@ sizePicker.submit(function(event){
   //create the canvas table
   makeGrid();
 
+  //add a listener for pick a background color button
+  button.click(function(event){
+    event.preventDefault;
+
+    //get the colors from background and background color picker in rbg format
+    backgroundColor = $('td').css('background-color');
+    let pickedColor = hexToRGB(colorPickerBackground.val());
+
+    //iterate trought every td element
+    $('td').each(function(event){
+      event.preventDefault;
+
+      //check if the background color is different from picked color and whether it is equal to the current background color
+      //if it is different set the backgound color to pickedColor
+      if($(this).css('background-color') !== pickedColor && $(this).css('background-color') === backgroundColor){
+        $(this).css('background-color', pickedColor);
+
+        // check if pickedColor is white if it is set the borders to black otherwise set them to #dbfffe
+        if(pickedColor === 'rgb(255, 255, 255)'){
+          $(this).css('border','#000000 solid 1px');
+        }else{
+          $(this).css('border','#dbfffe solid 1px');
+        }
+      }
+    });
+  });
+
   //add a listener to all <td> elements in HTML
   $('td').click(function(event){
 
-    //set the helper clickedCell, get the color from colorPicker and the current color from the clicked <td> and save it in a variable
+    //set the helper clickedCell, get the color from colorPicker, colorPickerBackground and the current color from the clicked <td> and save it in a variable
     let clickedCell = event.target;
+    backgroundColor = $('td').css('background-color');
     let color = hexToRGB(colorPicker.val());
     let current_color = $(clickedCell).css('background-color');
 
-    // check if we're trying to click on the <td> with the same color that's in it, if we are restore the background color and border color to basic
+    // check if we're trying to click on the <td> with the same color that's in it, if we are restore the background color and border color to background colors
+    // otherwise  set the background color of the <td> to color and the border to #dbfffe
     if (current_color === color){
-      $(clickedCell).css('background-color', '#ffffff');
-      $(clickedCell).css('border','black solid 1px');
-    }else {
+      $(clickedCell).css('background-color', backgroundColor);
 
-      //set the background color of the <td> to color and the border to #dbfffe
+        //set the border color to black if backgroundColor is white otherwise set it to #dbfffe
+        $(clickedCell).css('border', (backgroundColor==='rgb(255, 255, 255)' ? '#000000' : '#dbfffe') + ' solid 1px');
+    }else{
       $(clickedCell).css('background-color', color);
       $(clickedCell).css('border','#dbfffe solid 1px');
     }
